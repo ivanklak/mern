8b40418a71182dfda69754553a7b2b46c04a70ee
ivanklak <ivanklak17@gmail.com>
2022-01-31 15:21:41 +0300

    refactor: add registration with notification

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useHttp} from "../hooks/http.hook";
+import {useMessage} from "../hooks/message.hook";
 
 const AuthPage = () => {
+    const message = useMessage();
+    const {loading, error, request, clearError} = useHttp();
+    const [form, setForm] = useState({email: '', password: ''});
+
+    useEffect(() => {
+        message(error)
+        clearError()
+    }, [error, message, clearError]);
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form})
+            message(data.message)
+        } catch (err) {
+
+        }
+    }
+
     return (
         <div className="row">
             <div className="col s6 offset-s3">
@@ -14,7 +38,9 @@ const AuthPage = () => {
                                     placeholder="Введите email"
                                     id="email"
                                     type="text"
+                                    name="email"
                                     className="yellow-input"
+                                    onChange={changeHandler}
                                 />
                                 <label htmlFor="email">Email</label>
                             </div>
@@ -22,15 +48,25 @@ const AuthPage = () => {
                                 <input
                                     placeholder="Введите пароль"
                                     id="password"
+                                    name="password"
                                     type="password"
+                                    onChange={changeHandler}
                                 />
                                 <label htmlFor="password">Password</label>
                             </div>
                         </div>
                     </div>
                     <div className="card-action">
-                        <button className="btn black white-text" style={{marginRight: 20}}>Войти</button>
-                        <button className="btn white lighten-1 black-text">Регистрация</button>
+                        <button className="btn black white-text"
+                                style={{marginRight: 20}}
+                                disabled={loading}
+                        >Войти
+                        </button>
+                        <button className="btn white lighten-1 black-text"
+                                onClick={registerHandler}
+                                disabled={loading}
+                        >Регистрация
+                        </button>
                     </div>
                 </div>
 
