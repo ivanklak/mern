84619bae4cb01fd6e88aaca5f2371d051fb1a4a6
ivanklak <ivanklak17@gmail.com>
2022-02-02 16:17:19 +0300

    refactor: add Details page, fetch link by linkId

--- client/src/pages/DetailsPage.js
+++ client/src/pages/DetailsPage.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, {useCallback, useContext, useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+
+import {useHttp} from "../hooks/http.hook";
+import {AuthContext} from "../context/AuthContext";
+import Loader from "../components/Loader";
+import LinkCard from "../components/LinkCard";
 
 const DetailsPage = () => {
+    const {token} = useContext(AuthContext);
+    const {request, loading} = useHttp();
+    const [link, setLink] = useState(null);
+    const linkId = useParams().id;
+
+    const getLink = useCallback(async () => {
+        try {
+            const fetched = await request(`/api/link/${linkId}`, 'GET', null, {
+                Authorization: `Bearer ${token}`
+            });
+            setLink(fetched);
+        } catch (err) {}
+    }, [token, linkId, request])
+
+    useEffect(() => {
+        getLink()
+    },[getLink])
+
+    if (loading) {
+        return <Loader />
+    }
+
     return (
-        <div>
-            Details page
-        </div>
+        <>
+            { !loading && link && <LinkCard link={link} />}
+        </>
     )
 }
 
